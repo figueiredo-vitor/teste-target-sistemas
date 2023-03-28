a4664fa90a2b77055e1ec15778f82dc7879d68ac
Vitor Figueiredo <114297082+figueiredo-vitor@users.noreply.github.com>
2023-03-27 23:57:34 -0300

    Update string.py

--- string.py
+++ string.py
@@ -5,18 +5,22 @@
 # b) Evite usar funções prontas, como, por exemplo, reverse;
 
 
-#string a inverter
-string = '.mim arap edadinutropo amu arba e otrec ed euq orepse ' \
-         ',sovitejbo son odacof erpmes ,aid adac a rohlem res ocsuB'
+string = input("Digite uma Palavra para inverter: ")
 
-# converter o string em lista
-lista = list(string)
+# Convertendo a string em uma lista de caracteres
+lista_chars = list(string)
 
-# inverter ordem da lista
-lista.reverse()
+# Definindo os índices iniciais e finais para a inversão
+iniciais = 0
+finais = len(lista_chars) - 1
 
-#nova string  com a lista invertida
-string_reverse = ''.join(lista)
+# Invertendo os caracteres utilizando uma estratégia de dois ponteiros
+while iniciais < finais:
+    lista_chars[iniciais], lista_chars[finais] = lista_chars[finais], lista_chars[iniciais]
+    iniciais += 1
+    finais -= 1
 
-# immprimir string invertida
-print(string_reverse)
+# Convertendo a lista de caracteres de volta para uma string
+string_invertida = "".join(lista_chars)
+
+print(string_invertida)
